2e4f76c695a563fa911edb129d3aea4eee13ab67
David Cao <david.c.h.cao@gmail.com>
2016-10-16 01:06:02 -0300

    Version 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Minmay.Mixfile do
   def project do
     [
       app: :minmay,
-      version: "1.0.3",
+      version: "1.1.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
